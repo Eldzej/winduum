88ae1ad75665bc69d25cefebc6a59f3dfb7ae002
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2024-04-04 19:12:11 +0200

    feat: tw4 test

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
 import { resolve } from 'path'
-import tailwindcss from 'tailwindcss'
-import tailwindcssNesting from 'tailwindcss/nesting'
-import autoprefixer from 'autoprefixer'
-import postcssImport from 'postcss-import'
-import postcssNesting from 'postcss-nesting'
-import postcssCustomMedia from 'postcss-custom-media'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 
 export default {
     plugins: [
+        tailwindcss(),
         vue()
     ],
-    css: {
-        postcss: {
-            plugins: [postcssImport, tailwindcssNesting(postcssNesting), postcssCustomMedia, tailwindcss, autoprefixer]
-        }
-    },
     resolve: {
         alias: {
             '/src': resolve(process.cwd(), 'src')
